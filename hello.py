bdfbe510e80409dec06515f07068427b8a56fe48
cjy <cjy@169-231-132-162.wireless.ucsb.edu>
2018-10-17 09:33:52 -0700

    fix bugges in program

--- hello.py
+++ hello.py
@@ -22,27 +22,27 @@ def render_mtokm():
 @app.route("/ftoc_result")
 def render_ftoc_result():
     try:
-        ftemp_result = float(request.argss["fTemp"])
+        ftemp_result = float(request.args["fTemp"])
         ctemp_result = ftoc(ftemp_result)
-        return render_template("ftoc_result.html, fTemp=ftemp_result, cTemp=ctemp_result")
+        return render_template("ftoc_result.html", fTemp=ftemp_result, cTemp=ctemp_result)
     except ValueError:
         return "Sorry: something went wrong."
 
 @app.route("/ctof_result")
 def render_ctof_result():
     try:
-        ctemp_result = float(request.argss["cTemp"])
+        ctemp_result = float(request.args["cTemp"])
         ftemp_result = ctof(ctemp_result)
-        return render_template("ctof_result.html, cTemp=ctemp_result, fTemp=ftemp_result")
+        return render_template("ctof_result.html", cTemp=ctemp_result, fTemp=ftemp_result)
     except ValueError:
         return "Sorry: something went wrong."
 
 @app.route("/mtokm_result")
 def render_mtokm_result():
     try:
-        miles_result = float(request.argss["Miles"])
+        miles_result = float(request.args["Miles"])
         kms_result = mtokm(miles_result)
-        return render_template("mtokm_result.html, Miles=miels_result, Kms=kms_result")
+        return render_template("mtokm_result.html", Miles=miles_result, Kms=kms_result)
     except ValueError:
         return "Sorry: something went wrong."
 
